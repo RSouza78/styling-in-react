4aaa3c5e00d12363407591bc4fef818affebdf34
Rodrigo Acacio <rodrigoacaciovs@gmail.com>
2025-01-21 07:58:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Text from './components/text/text'
+
+function App() {
+
+  return (
+    <>
+<Text style={{textTransform: 'uppercase', color: 'red', fontWeight: 'bold'}}/>
+</>
+  )
+}
+
+export default App
---
+++ src/components/text/text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const text = ({style}) => {
+    return (
+        <div>
+            <p style={style}>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
+
+export default text
